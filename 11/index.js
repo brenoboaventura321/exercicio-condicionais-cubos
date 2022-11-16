55d39928968318d2227e9b12f86132076e8fb782
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 18:59:33 -0300

    exercicio 11

--- 11/index.js
+++ 11/index.js
@@ -6,3 +6,13 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+let valorParcela=0;
+
+if (rendaMensalEmCentavos>= 200000 && mesesDecorridos<60 && totalJaPagoPeloAluno<1800000){
+    valorParcela= 0.18*rendaMensalEmCentavos;
+    console.log(`valor da parcela é de ${valorParcela/100} reais`);
+}else{
+    valorParcela=0
+    console.log(`valor da parcela é de ${valorParcela}`);
+}
