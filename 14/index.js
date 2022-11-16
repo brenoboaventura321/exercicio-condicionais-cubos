6ed0eef87df396fda5ce90912ad8d09b2a5e468d
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 19:27:04 -0300

    exercicio 14

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,13 @@
 //quantidade de água ingerida em litro.
 const quantidadeDeAguaIngerida = 2;
 
+let risco="";
+
+if(quantidadeDeAguaIngerida<1.5)
+    risco="ALTO - Você está ingerindo pouquissima água, beba mais água!"
+else if(quantidadeDeAguaIngerida<3)
+    risco="MODERADO - Você está ingerindo pouca água, beba mais!"
+else
+    risco="BAIXO - Você está ingerindo uma boa quantidade de água, parabéns!"
+
+console.log(`Risco ${risco}`)
