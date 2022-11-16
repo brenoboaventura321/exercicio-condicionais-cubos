fb9d99ad88ec74d128dbf432f02d4fa76f0cac8b
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 19:41:49 -0300

    exercicio 15

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,18 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if(!primeiroNome){
+    console.log("pimeiro nome é obrigatorio")
+
+}else if(sobrenome){
+    if(!apelido){
+    console.log(primeiroNome+sobrenome);
+
+    }else{
+        console.log(`${primeiroNome} ${sobrenome} e de apelido ${apelido}`);
+    
+    }
+}else{
+    console.log(primeiroNome);
+}
+
