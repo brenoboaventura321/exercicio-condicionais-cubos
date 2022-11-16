7ae7fafda4e6c7c2cb97296c7ff7ba6848eae27b
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-16 10:46:54 -0300

    Exercicio 07

--- 07/index.js
+++ 07/index.js
@@ -3,3 +3,13 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+if (totalDeRendimentos>28_559.70){
+    if(aposentada || portadoraDeDoenca){
+        console.log("ISENTA");
+    }else{
+        console.log("PEGA LEAO");
+        console.log("CORRE MENINO NEY");
+    }
+}else{
+    console.log("VAZA LEAO! JA TA DIFICIO SEM VOCE");
+}
