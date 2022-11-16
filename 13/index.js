6e304d3b2de7a161098ace66cd50883409dbde5c
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 19:16:36 -0300

    exercicio 13*

--- 13/index.js
+++ 13/index.js
@@ -4,3 +4,13 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if (tipoDePagamento==="credito"){
+    let valorFinal= valorDoProduto-(valorDoProduto*0.05);
+    console.log(`o valor do produto com desconto é de R$ ${valorFinal/100}`);
+}else if(tipoDePagamento==="cheque"){
+    let valorFinal= valorDoProduto-(valorDoProduto*0.03);
+    console.log(`o valor do produto com desconto é de R$ ${valorFinal/100}`);
+}else{
+    let valorFinal= valorDoProduto-(valorDoProduto*0.10);
+    console.log(`o valor do produto com desconto é de R$ ${valorFinal/100}`);
+}
