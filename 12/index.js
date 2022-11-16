93948e1d12cf76c20955dfa09b1ce4c2500e1ffc
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 19:06:26 -0300

    exercicio 12

--- 12/index.js
+++ 12/index.js
@@ -1,1 +1,21 @@
 const diaDaSemana = 3;
+
+if(diaDaSemana>=1 && diaDaSemana<=7){
+    if(diaDaSemana===1){
+        console.log("Segunda Feira")
+    }else if(diaDaSemana===2){
+        console.log("Terça Feira")
+    }else if(diaDaSemana===3){
+        console.log("Quarta Feira")
+    }else if(diaDaSemana===4){
+        console.log("Quinta Feira")
+    }else if(diaDaSemana===5){
+        console.log("Sexta Feira")
+    }else if(diaDaSemana===5){
+        console.log("Sabado")
+    }else{
+        console.log("Domingo")
+    }
+}else{
+    console.log("numero invalido");
+}
