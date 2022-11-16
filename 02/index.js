4aeb3e0593284376f338faa0444188ca9a4cb479
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-15 23:42:34 -0300

    Exercicio 02

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,7 @@
-const jogada1 = 5;
+const jogada1 = 4;
 const jogada2 = 3;
 
 //seu código aqui
+
+const valor = ((jogada1 + jogada2)%2 === 0 ? "par" : "impar");
+console.log(`O numero é ${valor}`);
