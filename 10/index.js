2e24a6b1322b293efe58a16b6f16e22f018acd36
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-16 12:36:26 -0300

    exercicio 10

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,16 @@
-const caractere = "E";
+const caractere = "A";
+
+if(typeof caractere=== "string"){
+    if (caractere ==="A"||caractere === "E" ||caractere ==="I"||caractere === "O"|| caractere ==="U"|| caractere ==="a"||caractere === "e"||caractere === "i"||caractere === "o"||caractere === "u"){
+        console.log("VOGAL");
+        if(caractere === caractere.toUpperCase()){
+            console.log("maiuscula");
+        }else{
+            console.log("Minuscula");
+        }
+    }else{
+        console.log("CONSOANTE");
+    }
+}else{
+    console.log("NUMERO");
+}
