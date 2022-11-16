379fc2ebd335f6f5584af6af7850fc36da0748f7
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-16 10:35:27 -0300

    exercicio 06

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,16 @@
-const alturaEmCm = 185;
+const alturaEmCm = 188;
 
 //seu código aqui
+if (alturaEmCm>=180){
+    if(alturaEmCm<=185){
+        console.log("líbero");
+    }else if(alturaEmCm<=195){
+        console.log("ponteiro");
+    }else if(alturaEmCm<=205){
+        console.log("oposto");
+    }else{
+        console.log("central");
+    }
+}else{
+    console.log("REPROVADO");
+}
