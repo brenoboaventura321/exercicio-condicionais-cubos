4c29c378a4bdf06f78d830c87fa5c468cbe5b3d5
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-16 11:42:49 -0300

    exercicio 09

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,21 @@
 const nota = 8.5;
+
+let conceito="";
+if(nota>=0 && nota<=10){
+    if (nota>4){
+        if(nota<5.9)
+            conceito="D"
+        else if(nota<=7.9)
+            conceito= "C"
+        else if(nota<=8.9)
+            conceito= "B"
+        else
+            conceito="A"
+        console.log(`O estudante obteve conceito ${conceito}`)
+
+    }else{
+        console.log(`O estudante obteve conceito ${conceito}`);
+    }
+}else{
+    console.log("nota invalida");
+}
