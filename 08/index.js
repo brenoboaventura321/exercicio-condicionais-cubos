a3673293ec7124db4a454e1268da60f923f2a3c1
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-16 11:00:58 -0300

    exercicio 08

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,14 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+const permicaoIdade=(idade>= 12 && idade<=65 && !possuiPatologia ? true : false);
+
+if (permicaoIdade){
+    if (ehEstudante){
+        console.log("10 reais");
+    }else{
+        console.log("20 reais");
+    }
+}else{
+    console.log("ACESSO NEGADO");
+}
